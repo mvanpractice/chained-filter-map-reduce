228dc3c9624b9ee5410f7929672c5c5ec5102d90
mvanpractice <michaelmanggay@gmail.com>
2025-06-10 10:07:04 +0800

    Second solution but shortened a bit

--- camelizedString-exercise.js
+++ camelizedString-exercise.js
@@ -4,14 +4,18 @@
 */
 function camelizeString(str) {
 
-    const newStrArray = str.split('-');
+    // const newStrArray = str.split('-');
 
-    return newStrArray.map((item, index) => {
-        if (index === 0) {
-            return item;
-        } else {
-            return item[0].toUpperCase() + item.slice(1);
-        }
+    // return newStrArray.map((item, index) => {
+    //     if (index === 0) {
+    //         return item;
+    //     } else {
+    //         return item[0].toUpperCase() + item.slice(1);
+    //     }
+    // }).join('');
+
+    return str.split('-').map((text, index) => {
+        return index === 0 ? text : text[0].toUpperCase() + text.slice(1);
     }).join('');
 
 }
