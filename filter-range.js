a04e1272b9aafedbfa9ab04a467f87cffb1e56b5
mvanpractice <michaelmanggay@gmail.com>
2025-06-10 10:25:02 +0800

    solution - first iteration

---
+++ filter-range.js
@@ -0,0 +1,13 @@
+
+function filterRange(arr, a, b) {
+    
+    return arr.filter((item) => {
+        return item >= a && item <= b;
+    });
+
+}
+
+const arr = [5,3,8,1];
+
+console.log(filterRange(arr,1,4));
+console.log(arr);
