9d3f34a08001a4113809c5673d289d46a16483e9
mvanpractice <michaelmanggay@gmail.com>
2025-06-10 09:51:02 +0800

    Repeating camelize exercise from TOP using map() for practice

---
+++ camelizedString-exercise.js
@@ -0,0 +1,13 @@
+
+/* 
+ @param str - String
+*/
+function camelizeString(str) {
+
+    const newStrArray = str.split('-');
+
+    console.log(newStrArray);
+
+}
+
+console.log(camelizeString('dah-bla-bl'));
